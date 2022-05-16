6b388646ca9c7c4438ee707d8a5be597dd486bd8
Eduardo Moraton <eduardo.moraton@iesdoctorbalmis.com>
2022-05-16 17:51:17 +0200

    avances

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Testeo from './Testeo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Testeo />
   </React.StrictMode>
 );
 
