9155bc2425f7f14e8ec7eecfc16b4d19b2364177
Eduardo Moraton <eduardo.moraton@iesdoctorbalmis.com>
2022-05-17 21:56:57 +0200

    avances

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -1,10 +1,25 @@
-const ItemInfo = ({open, data}) => {
-    if(!open) return null
-    return (
-        <div>
-            <h1>marcos</h1>
-        </div>
-    )
+import { ReactDOM } from "react";
+
+const MODAL_STYLES = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#FFF',
+    padding: '50px',
+    zIndex: 1000
 }
 
-export default ItemInfo;
+
+export default function ItemInfo({open, data}){
+    if(!open) return null
+    return ReactDOM.createPortal(
+        <>
+        <div style={MODAL_STYLES}>
+            <div className="btn-close-modal" onClick={open=false}>CLOSE</div>
+            <h1>marcos</h1>
+        </div>
+        </>,
+        Document.getElemetById("portal")
+    )
+}
