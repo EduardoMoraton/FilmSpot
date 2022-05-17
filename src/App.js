c9b46fb2494810a77be3d3b423112b91f8d1f3a6
Marcxsvss <90453251+Marcxsvss@users.noreply.github.com>
2022-05-17 19:31:24 +0200

    Bootstrap Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import List from './components/List'
+import Header from './components/Header'
 
 function App() {
   return (
 
     <div className="app">
+      <Header />
       <List />
     </div>
   )
