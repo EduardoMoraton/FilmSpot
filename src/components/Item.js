37afbc70b3468d2caccebf09b960b876f42529de
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-05-18 10:40:50 -0700

    cambios y iteminfo funcional

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,11 +6,10 @@ function Item({data}) {
     const [isOpen, setIsOpen] = useState(false)
     let imgSrc = "https://image.tmdb.org/t/p/w500"
     return (
-        <div onClick={()=> setIsOpen(!isOpen)}>
-            <h1>{data.title ?? "Undefined"}</h1>
-            <p>{data.overview}</p>
+        <div onClick={()=> setIsOpen(!isOpen)} className="item">
+            <p>{data.title ?? "Undefined"}</p>
             <img src={imgSrc+data.poster_path}></img>
-            <ItemInfo open={isOpen}/>
+            <ItemInfo open={isOpen} info={data}/>
         </div>
     )
 }
--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -1,26 +1,42 @@
-import { ReactDOM } from "react";
+import ReactDOM from 'react';
 
 const MODAL_STYLES = {
+    color: "FFF",
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    backgroundColor: '#FFF',
-    padding: '50px',
-    zIndex: 1000
+    backgroundColor: 'rgba(0, 0, 0, 1)',
+    zIndex: 1000,
+    transition: "4s"
+}
+
+const OUT_MODAL_STYLES = {
+    color: "FFF",
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'rgba(0, 0, 0, .5)',
+    zIndex: 1000,
+    width: "100%",
+    height: "100%",
+    transition: "3s"
 }
 
 
-export default function ItemInfo({open, data}){
-    
+export default function ItemInfo({open, info}){
+    console.log(info)
     if(!open) return null
-    return ReactDOM.createPortal (
+    return (
         <>
-        <div style={MODAL_STYLES}>
-            <div className="btn-close-modal" onClick={open=false}>CLOSE</div>
-            <h1>marcos</h1>
+        <div style={OUT_MODAL_STYLES} className="item-info">
+            <div style={MODAL_STYLES}>
+                <div className="btn-close-modal" onClick={open=false}>CLOSE</div>
+                <h1>{info.title}</h1>
+                <p>{info.overview}</p>
+            </div>
         </div>
-        </>,
-        document.getElemetById("portal")
+        </>
     )
 }
