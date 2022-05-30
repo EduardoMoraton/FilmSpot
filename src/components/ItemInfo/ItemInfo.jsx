2d012c9155b0573afd10b0533c1b34574cf62898
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-05-30 04:57:59 -0700

    avance

--- src/components/ItemInfo/ItemInfo.jsx
+++ src/components/ItemInfo/ItemInfo.jsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom';
 import './ItemInfo.css'
 import '../RatingRadios/RatingRadio.jsx'
 import RaitingRadio from '../RatingRadios/RatingRadio.jsx';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 // https://animate.style/
 
 export function ItemInfo({open, info}){
-    let imgSrc = "https://image.tmdb.org/t/p/w500"
+    let imgSrc = "https://image.tmdb.org/t/p/original"
+    let imgSrcLow = "https://image.tmdb.org/t/p/w500"
     if(!open) return null
     return (ReactDOM.createPortal(
         <div className="item-info-out animate__animated animate__fadeIn animate__faster">
@@ -14,7 +16,12 @@ export function ItemInfo({open, info}){
                 .
                 <div className="btn-close-modal" onClick={open=false}>CLOSE</div>
                 <RaitingRadio rating={info.vote_average} />
-                <img src={imgSrc+info.poster_path}></img>
+
+                <LazyLoadImage 
+                    src={imgSrc+info.backdrop_path}
+                    placeholderSrc={imgSrcLow+info.backdrop_path}
+                    effect="blur">
+                </LazyLoadImage>
                 <div className='info-text'>
                     <h1 clasNmae="animate__animated animate__fadeInUp animate__slow">{info.title ?? info.name}</h1>
                     <p  clasNmae="animate__animated animate__fadeInUp animate__slower">{info.overview}</p>
